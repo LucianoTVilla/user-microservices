e135c286b2f76a99ab789f186dee2d2e2be124ae
LucianoTVilla <lucianotomasvilla@gmail.com>
2023-04-20 21:24:11 -0300

    [FIX]: Move sensitive vars to .env

--- login/src/db/connection.js
+++ login/src/db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const { username, password } = require('../utils/constants');
 
-const uri = process.env.MONGODB_URI || `mongodb+srv://luchotomasvilla:test123@cluster0.y4zbd0n.mongodb.net/conexa?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI || `mongodb+srv://${username}:${password}@cluster0.y4zbd0n.mongodb.net/conexa?retryWrites=true&w=majority`;
 
 module.exports = () => mongoose.connect(uri, {
   useNewUrlParser: true,
--- login/src/utils/constants.js
+++ login/src/utils/constants.js
@@ -1,5 +1,9 @@
 const jwtSecret = process.env.JWT_SECRET;
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
 
 module.exports = {
   jwtSecret,
+  username,
+  password,
 };
