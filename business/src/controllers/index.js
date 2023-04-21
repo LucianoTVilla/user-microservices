aae1b7b3163912671511be2cd0ea52872d58e185
LucianoTVilla <lucianotomasvilla@gmail.com>
2023-04-20 22:37:40 -0300

    [FEAT]: Add pagination and avoid password field

--- business/src/controllers/index.js
+++ business/src/controllers/index.js
@@ -10,8 +10,26 @@ const getUsers = async (req, res) => {
   }
 
   try {
-    const users = await User.find();
-    res.status(200).send(users);
+    let { page, limit, search } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+    search = search ? search.toLowerCase() : '';
+    const skip = (page - 1) * limit;
+    const query = {
+      email: { $regex: new RegExp(search, 'i') },
+    };
+    const count = await User.countDocuments(query);
+    const users = await User.find(query)
+      .skip(skip)
+      .limit(limit)
+      .select('_id email')
+
+    res.status(200).send({
+      page,
+      limit,
+      total: count,
+      users,
+    });
   } catch (error) {
     res.status(500).send({
       message: 'Error getting users',
